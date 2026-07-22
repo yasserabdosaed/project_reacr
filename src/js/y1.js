9fc3df27f1cae3b6d5d080a0d35b13765315cb6b
yasserabdosaed <yy3882603@gmail.com>
2026-07-22 15:52:42 +0300

    project react 1.1

--- src/js/y1.js
+++ src/js/y1.js
@@ -5,14 +5,20 @@ let nem="yasser";
 
 
 export default  function Y1(){
+    
     let ss = useState(nem)
     let vl = ss['0']
     let st=ss["1"]
   console.log(vl)
 
      function nn(){
+        if(vl === "yasser"){
+ st("ali")
+        }else{
+            st("yasser")
+        }
  console.log(vl)
-     st("ali")
+    
       console.log(vl)
     }
 
