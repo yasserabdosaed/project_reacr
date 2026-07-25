85baccf28dea3a5aa97aad32e468748aec1e8fc7
yasserabdosaed <yy3882603@gmail.com>
2026-07-25 12:06:08 +0300

    project react 1.4

--- src/js/y1.js
+++ src/js/y1.js
@@ -1,40 +1,71 @@
 import { useState } from "react";
 
-export default function FOyas(){
+export default function Ha(params) {
+  
+  let [allgt, allst] = useState({
+    x:0,
+    name:"",
+    emile:"",
+    chk:false
+  });
 
-    let [allgt,allst]= useState({im:"",pas:""})
+  return (
+    <div
+      style={{
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+            padding:"10px",
+          width: "400px",
+          height: "200px",
+          backgroundColor: "white",
+          margin: "20px auto",
+        }}
+      >
+        <h3
+          style={{
+            display: "inline-block",
+            marginRight: "10px",
+          }}
+        >
+          scro:{allgt.x}{" "}
+        </h3>
+        <button onClick={(ee) => {
+          
+          
+            allst({...allgt,x:allgt.x + 1});
+            
+        }}>+1</button>
 
-    return(
-        <form onSubmit={(f)=>{
-f.preventDefault()
+        <form onSubmit={(n)=>{
+n.preventDefault()
+console.log(allgt)
         }}>
+     
+     username : <input value={allgt.name} onChange={(nn)=>{
+allst({...allgt,name:nn.target.value})
+     }}/>
+     <br/>
+     gimail : <input value={allgt.emile} onChange={(ii)=>{
+allst({...allgt,emile:ii.target.value})
+     }}/>
+     <input onChange={(c)=>{
+console.log(c.target.checked)
+allst({...allgt,chk:c.target.checked
 
-<input value={allgt.im} onChange={(emm)=>{
-allst({...allgt,im:emm.target.value})
-}}/>
-<br/>
-<input value={allgt.pas} onChange={(paa)=>{
-allst({...allgt,pas:paa.target.value})
-}}/>
-<br/>
+})
 
-<button>OK</button>
 
-<div style={{
-    border:"2px solid red"
-}}>
+     }} type="checkbox" />
 
-<p>{allgt.im}</p>
-<p>{allgt.pas}</p>
+     <button>OK</button>
 
-</div>
+
 
         </form>
-    );
-
-    
-
-   
-
-
+      </div>
+    </div>
+  );
 }
